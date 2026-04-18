5b9f70f5485033613d29815dec6703b2e0ff75c3
Dolly <dollyd8254@gmail.com>
2026-04-18 20:36:35 +0530

    Fixed duplicate code after merge

--- analysis.py
+++ analysis.py
@@ -36,35 +36,4 @@ print("\nTopper:\n", topper)
 # Graph
 city_avg.plot(kind="bar")
 plt.show()
-=======
-import pandas as pd
 
-# Read data
-df = pd.read_csv("students.csv")
-
-# Show data
-print("Full Data:\n", df)
-print("-"*30)
-
-# Pass students
-pass_students = df[df["Marks"] >= 50]
-print("Pass students:\n", pass_students)
-print("-"*30)
-
-# Fail students
-fail_students = df[df["Marks"] < 50]
-print("Fail students:\n", fail_students)
-print("-"*30)
-
-# Average marks
-print("Average Marks (Pass):", pass_students["Marks"].mean())
-print("Average Marks (Fail):", fail_students["Marks"].mean())
-print("-"*30)
-
-# City-wise average
-print("City-wise Average:\n", df.groupby("City")["Marks"].mean())
-print("-"*30)
-
-# City-wise student count
-print("Students per City:\n", df.groupby("City")["Name"].count())
->>>>>>> 6527c4dff23a917adebc053771c64b5732af8092
